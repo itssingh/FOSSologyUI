81fc8330f36486a7bfd53b9c8d7c735b82545de3
Aman Dwivedi <aman.dwivedi5@gmail.com>
2021-07-04 10:15:50 +0530

    fix(license): add missing license header to files
    Signed-off-by: Aman Dwivedi <aman.dwivedi5@gmail.com>

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,5 +1,5 @@
 /*
- Copyright (C) 2021 Shruti Agarwal (mail2shruti.ag@gmail.com)
+ Copyright (C) 2021 Shruti Agarwal (mail2shruti.ag@gmail.com), Aman Dwivedi (aman.dwivedi5@gmail.com)
  
  SPDX-License-Identifier: GPL-2.0
 
